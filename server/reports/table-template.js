626ffb2e9ed698f9a57b24492a1c0dedbf4ee33a
TSU HSIN YEH <tyeh3@IDHFJSNF.local>
2018-11-04 16:21:08 +0800

    Created table-template for PDF generation. Completed active-payment report.

---
+++ server/reports/table-template.js
@@ -0,0 +1,82 @@
+// PDF Printer
+const PdfPrinter = require('pdfmake/src/printer');
+
+// Constants Font Object for the PDF
+const fonts = {
+    Roboto: {
+        normal: './asset/fonts/Roboto-Regular.ttf',
+        bold: './asset/fonts/Roboto-Medium.ttf',
+        italics: './asset/fonts/Roboto-Italic.ttf',
+        bolditalics: './asset/fonts/Roboto-MediumItalic.ttf'
+    },
+    Chinese: {
+        normal: './asset/fonts/msyh.ttf',
+        bold: './asset/fonts/msyh.ttf',
+        italics: './asset/fonts/msyh.ttf',
+        bolditalics: './asset/fonts/msyh.ttf',
+    }
+};
+
+const getTablePDFDocument = (headers, rows) => {
+
+    var printer = new PdfPrinter(fonts);
+
+    headers.forEach((element, index) => {
+        headers[index]['style'] = 'tableHeader';
+    });
+
+    var data = [ headers ];
+
+    for(i = 0; i < rows.length; i++) {
+        data.push(rows[i]);
+    }
+
+    var docDefinition = {
+        content: [
+            { text: '付款清單', style: 'header'},
+            {
+                style: 'tableExample',
+                table: {
+                    widths: ['*', '*', '*', '*', '*', '*'],
+                    headerRows: 1,
+                    body: data
+                },
+                layout: {
+                    fillColor: function (rowIndex, node, columnIndex) {
+                        if(rowIndex === 0) return '#4c90ff';
+                        return (rowIndex % 2 === 0) ? '#eff5ff' : null;
+                    }
+                }
+            }
+        ],
+        styles: {
+            header: {
+                fontSize: 18,
+                bold: true,
+                margin: [0, 0, 0, 10]
+            },
+            subheader: {
+                fontSize: 16,
+                bold: true,
+                margin: [0, 10, 0, 5]
+            },
+            tableExample: {
+                margin: [0, 5, 0, 15]
+            },
+            tableHeader: {
+                bold: true,
+                fontSize: 13,
+                color: 'black'
+            }
+        },
+        defaultStyle: {
+            font: 'Chinese'
+        }
+    };
+
+    return printer.createPdfKitDocument(docDefinition);
+};
+
+module.exports = {
+    getTablePDFDocument
+};
