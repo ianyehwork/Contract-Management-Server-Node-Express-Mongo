db321da98b145cc2c1de64a8492fc607992598db
TSU HSIN YEH <tyeh3@IDHFJSNF.local>
2018-11-09 16:02:37 +0800

    Finished the server-size pagination for customer and parking area.

--- server/controllers/customer-api.js
+++ server/controllers/customer-api.js
@@ -2,22 +2,22 @@
  * This file represents the REST API for Customer collection.
  */
 const _ = require('lodash');
-const {ObjectID} = require('mongodb');
-var {Customer} = require('./../models/customer');
-const {generateLocalDateTime} = require('./../util/utility');
+const { ObjectID } = require('mongodb');
+var { Customer } = require('./../models/customer');
+const { generateLocalDateTime } = require('./../util/utility');
 const url = require('url');
 
 const CUSTOMER_POST_API = (request, response) => {
     var body = _.pick(request.body, [
-        'pContact', 
-        'sContact', 
+        'pContact',
+        'sContact',
         'pPhone',
         'sPhone',
         'address'
     ]);
     var model = new Customer(body);
-    _.forEach(request.body.vehicles, function(value) {
-        model.vehicles.push({vin: _.toString(value)});
+    _.forEach(request.body.vehicles, function (value) {
+        model.vehicles.push({ vin: _.toString(value) });
     });
     model.dateCreated = generateLocalDateTime();
     model.dateModified = generateLocalDateTime();
@@ -30,27 +30,42 @@ const CUSTOMER_POST_API = (request, response) => {
 
 const CUSTOMER_GET_API = (request, response) => {
     var queryData = url.parse(request.url, true).query;
+    queryData.page = _.toInteger(queryData.page);
+    queryData.pageSize = _.toInteger(queryData.pageSize);
+
     var filter = {};
-    if(queryData.startWith) {
-        filter.pContact = {$regex : "^" + queryData.startWith};
+    if (queryData.field && queryData.match) {
+        filter[queryData.field] = { $regex: "^" + queryData.match };
     }
-    Customer.find(filter).then((model) => {
-        response.send(model); 
-    }, (err) => {
-        response.status(400).send(err);
-    });
+
+    // Convert String to Object Property using []
+    const query = Customer.find(filter)
+        .sort({ [queryData.order]: (queryData.reverse ? -1 : 1 )})
+        .skip((queryData.page - 1) * queryData.pageSize)
+        .limit(queryData.pageSize);
+
+    Promise.all([query, Customer.find(filter).countDocuments()])
+        .then((results) => {
+            response.send({
+                data: results[0],
+                collectionSize: results[1]
+            });
+        }).catch((err) => {
+            console.log(err);
+            response.status(500).send();
+        });
 };
 
 const CUSTOMER_GET_ID_API = (request, response) => {
     var id = request.params.id;
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return response.status(404).send();
     }
 
     Customer.findOne({
         _id: id,
     }).then((model) => {
-        if(!model) {
+        if (!model) {
             return response.status(404).send();
         }
         return response.send(model);
@@ -61,14 +76,14 @@ const CUSTOMER_GET_ID_API = (request, response) => {
 
 const CUSTOMER_DELETE_API = (request, response) => {
     var id = request.params.id;
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return response.status(404).send();
     }
 
     Customer.findOneAndRemove({
         _id: id
     }).then((model) => {
-        if(!model) {
+        if (!model) {
             return response.status(404).send();
         }
         return response.send(model);
@@ -81,33 +96,33 @@ const CUSTOMER_PATCH_API = (request, response) => {
     var id = request.params.id;
     // Only extract the properties required if exist
     var body = _.pick(request.body, [
-        'pContact', 
-        'sContact', 
+        'pContact',
+        'sContact',
         'pPhone',
         'sPhone',
         'address'
     ]);
 
     body.vehicles = [];
-    _.forEach(request.body.vehicles, function(value) {
-        body.vehicles.push({vin: _.toString(value)});
+    _.forEach(request.body.vehicles, function (value) {
+        body.vehicles.push({ vin: _.toString(value) });
     });
     body.dateModified = generateLocalDateTime();
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return response.status(404).send();
     }
 
-    Customer.findOneAndUpdate({ _id: id}, 
-                          {$set: body}, 
-                          {new: true}).then((model) => {
-        if(!model) {
-            return response.status(404).send();
-        }
-        return response.send(model);
-    }).catch((err) => {
-        response.status(400).send();
-    });
+    Customer.findOneAndUpdate({ _id: id },
+        { $set: body },
+        { new: true }).then((model) => {
+            if (!model) {
+                return response.status(404).send();
+            }
+            return response.send(model);
+        }).catch((err) => {
+            response.status(400).send();
+        });
 };
 
 module.exports = {
--- server/controllers/parking-area-api.js
+++ server/controllers/parking-area-api.js
@@ -2,20 +2,22 @@
  * This file represents the REST API for Parking Area collection.
  */
 const _ = require('lodash');
-const {ObjectID} = require('mongodb');
-var {ParkingArea} = require('./../models/parking-area');
-const {generateLocalDateTime} = require('./../util/utility');
+const { ObjectID } = require('mongodb');
+var { ParkingArea } = require('./../models/parking-area');
+const { generateLocalDateTime } = require('./../util/utility');
+const url = require('url');
+
 const PARKING_AREA_POST_API = (request, response) => {
     var body = _.pick(request.body, [
-        'identifier', 
+        'identifier',
         'address',
-        'defaultDeposit', 
+        'defaultDeposit',
         'defaultRent',
         'comment'
     ]);
     var model = new ParkingArea(body);
-    _.forEach(request.body.vehicles, function(value) {
-        model.vehicles.push({vin: _.toString(value)});
+    _.forEach(request.body.vehicles, function (value) {
+        model.vehicles.push({ vin: _.toString(value) });
     });
     model.dateCreated = generateLocalDateTime();
     model.dateModified = generateLocalDateTime();
@@ -27,23 +29,43 @@ const PARKING_AREA_POST_API = (request, response) => {
 };
 
 const PARKING_AREA_GET_API = (request, response) => {
-    ParkingArea.find({}).then((model) => {
-        response.send(model); 
-    }, (err) => {
-        response.status(400).send(err);
-    });
+    var queryData = url.parse(request.url, true).query;
+    queryData.page = _.toInteger(queryData.page);
+    queryData.pageSize = _.toInteger(queryData.pageSize);
+
+    var filter = {};
+    if (queryData.field && queryData.match) {
+        filter[queryData.field] = { $regex: "^" + queryData.match };
+    }
+
+    // Convert String to Object Property using []
+    const query = ParkingArea.find(filter)
+        .sort({ [queryData.order]: (queryData.reverse ? -1 : 1) })
+        .skip((queryData.page - 1) * queryData.pageSize)
+        .limit(queryData.pageSize);
+
+    Promise.all([query, ParkingArea.find(filter).countDocuments()])
+        .then((results) => {
+            response.send({
+                data: results[0],
+                collectionSize: results[1]
+            });
+        }).catch((err) => {
+            console.log(err);
+            response.status(500).send();
+        });
 };
 
 const PARKING_AREA_GET_ID_API = (request, response) => {
     var id = request.params.id;
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return response.status(404).send();
     }
 
     ParkingArea.findOne({
         _id: id,
     }).then((model) => {
-        if(!model) {
+        if (!model) {
             return response.status(404).send();
         }
         return response.send(model);
@@ -54,14 +76,14 @@ const PARKING_AREA_GET_ID_API = (request, response) => {
 
 const PARKING_AREA_DELETE_API = (request, response) => {
     var id = request.params.id;
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return response.status(404).send();
     }
 
     ParkingArea.findOneAndRemove({
         _id: id
     }).then((model) => {
-        if(!model) {
+        if (!model) {
             return response.status(404).send();
         }
         return response.send(model);
@@ -74,33 +96,33 @@ const PARKING_AREA_PATCH_API = (request, response) => {
     var id = request.params.id;
     // Only extract the properties required if exist
     var body = _.pick(request.body, [
-        'identifier', 
+        'identifier',
         'address',
-        'defaultDeposit', 
+        'defaultDeposit',
         'defaultRent',
         'comment'
     ]);
 
     body.vehicles = [];
-    _.forEach(request.body.vehicles, function(value) {
-        body.vehicles.push({vin: _.toString(value)});
+    _.forEach(request.body.vehicles, function (value) {
+        body.vehicles.push({ vin: _.toString(value) });
     });
     body.dateModified = generateLocalDateTime();
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return response.status(404).send();
     }
 
-    ParkingArea.findOneAndUpdate({ _id: id}, 
-                          {$set: body}, 
-                          {new: true}).then((model) => {
-        if(!model) {
-            return response.status(404).send();
-        }
-        return response.send(model);
-    }).catch((err) => {
-        response.status(400).send();
-    });
+    ParkingArea.findOneAndUpdate({ _id: id },
+        { $set: body },
+        { new: true }).then((model) => {
+            if (!model) {
+                return response.status(404).send();
+            }
+            return response.send(model);
+        }).catch((err) => {
+            response.status(400).send();
+        });
 };
 
 module.exports = {
