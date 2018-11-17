969339e9042fbac78c33486451b74afa8fa867a2
TSU HSIN YEH <tyeh3@IDHFJSNF.local>
2018-11-17 13:50:34 +0800

    Attempted to validate message validity (2)

--- server/controllers/line-api.js
+++ server/controllers/line-api.js
@@ -8,8 +8,9 @@ const _ = require('lodash');
  * @param {*} body request.body
  */
 const isSignatureValid = (headerSignature, requestBody) => {
-    const channelSecret = c06ce72aa9ba05542fa8a171b779a869; // Channel secret string
+    const channelSecret = 'c06ce72aa9ba05542fa8a171b779a869'; // Channel secret string
     const body = JSON.stringify(requestBody); // Request body string
+    console.log(body);
     const signature = crypto
         .createHmac('SHA256', channelSecret)
         .update(body).digest('base64');
@@ -20,7 +21,7 @@ const isSignatureValid = (headerSignature, requestBody) => {
 const WEBHOOK_POST_API = (request, response) => {
     console.log('request.headers');
     console.log(request.headers);
-    console.log(isSignatureValid(request.headers.x-line-signature) ? 'Valid Signature' : "Invalid Signature")
+    console.log(isSignatureValid(request.headers['x-line-signature'], request.body) ? 'Valid Signature' : "Invalid Signature")
     console.log('request.body');
     console.log(request.body);
     return response.send();
