900cbc65e45bfbf9c264a1bdc33941afbd8bb1cf
TSU HSIN YEH <tyeh3@IDHFJSNF.local>
2019-08-21 12:37:15 -0400

    Handled invalid line token entered by user.

--- server/line/line-controller.js
+++ server/line/line-controller.js
@@ -3,19 +3,22 @@ const _ = require('lodash');
 const line = require('@line/bot-sdk');
 const { Customer } = require('./../models/customer');
 const { Contract } = require('./../models/contract');
+const { ParkingLot } = require('./../models/parking-lot');
+const { Payment } = require('./../models/payment');
 const { LineToken } = require('./../models/line-token');
 
 const client = new line.Client({
     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 });
 
-const PLEASE_EMOJI = '\u{10007A}';
-const HAPPY_EMOJI = '\u{100090}';
-const IDENTITY_NOT_VERIFIED = '身份未驗證! 請輸入身份驗證碼(6位), 並用*結尾.' + PLEASE_EMOJI +' 例如: A82JuL*';
+// const PLEASE_EMOJI = '\u{10007A}';
+// const HAPPY_EMOJI = '\u{100090}';
+const IDENTITY_NOT_VERIFIED = '身份未驗證! 請輸入身份驗證碼(6位), 並用*結尾. 例如: A82JuL*';
 const IDENTITY_VERIFIED = '身份已驗證! 您的身份是: ';
 const SYSTEM_ERROR = '系統錯誤! 請通知管理員, 謝謝!';
 const CUSTOMER_TOKEN_NOT_EXISTS = '驗證碼不存在! 請輸入身份驗證碼(6位), 並用*結尾. 例如: A82JuL*';
 const MENU_INSTRUCTION = '指令清單:\n新用戶身分驗證請輸入 *1\n合同/付款查詢請輸入 *2'
+const NO_ACTIVE_CONTRACT = '找不到生效的合同！'
 
 /**
  * The signature in the X-Line-Signature request header must be
@@ -50,7 +53,7 @@ const processLineMessage = (data) => {
             // Identity Verification
             Customer.findOne({ lineUID: data['sourceUserId'] }).then((customer) => {
                 if (customer) {
-                    sendMessage(replyTokenValue, IDENTITY_VERIFIED + customer.pContact + '.' + HAPPY_EMOJI);
+                    sendMessage(replyTokenValue, IDENTITY_VERIFIED + customer.pContact + '.');
                 } else {
                     sendMessage(replyTokenValue, IDENTITY_NOT_VERIFIED);
                 }
@@ -63,16 +66,51 @@ const processLineMessage = (data) => {
                 if (!customer) {
                     sendMessage(replyTokenValue, IDENTITY_NOT_VERIFIED);
                 } else {
-                    Contract.find({_customer: customer._id}).then((contract) => {
-                        sendMessage(replyTokenValue, contract);
-                    });
+                    // Find the customer active contract
+                    var filter = { _customer: customer._id, active: true };
+                    Contract.find(filter).populate({
+                        path: '_lot',
+                        select: 'rent'
+                    }).then((contracts) => {
+                        if(!contracts) {
+                            sendMessage(replyTokenValue, NO_ACTIVE_CONTRACT);
+                        } else {
+                            for (i = 0; i < contracts.length; i++) {
+                                function getPayments(callback, contractId){
+                                    Payment.find({_contract: contractId}).then((payments) => {
+                                        callback(payments);
+                                    })
+                                }
 
-                    
+                                getPayments(function(payments){
+                                    var message = '';
+                                    message += '起租日期: ' + contracts[i].sYear + '年' + contracts[i].sMonth + '月' + contracts[i].sDay + '日\n';
+                                    message += '月租金: ' + contracts[i]._lot.rent + '\n';
+                                    message += '繳費週期: ' + contracts[i].pFrequency + '個月\n';
+                                    message += '下次繳費日期: ' + contracts[i].pYear + '年' + contracts[i].pMonth + '月' + contracts[i].pDay + '日\n';
+                                    message += '下次繳費金額: ' + (contracts[i].pFrequency * contracts[i]._lot.rent) + '\n';
+                                    
+                                    if(payments) {
+                                        message += '繳費紀錄:\n';
+                                        for (i = 0; i < payments.length; i++) {
+                                            var payment = '';
+                                            if(payments[i].type == 'R' || payments[i].type == 'D') {
+                                                var type = payments[i].type == 'R' ? '租金' : '押金';
+                                                payment += payments[i].dateCreated + ' ' + type + ' ' + payments[i].amount + '\n';
+                                                message += payment;
+                                            }
+                                        }
+                                    }
+                                    sendMessage(replyTokenValue, message);
+                                }, contracts._id);
+                            }
+                        }
+                    });
                 }
             }).catch((err) => {
                 sendMessage(replyTokenValue, SYSTEM_ERROR);
             });
-        } else if (_.toString(data['messageText']).match(/^[0-9a-zA-z]{6}\*/).length > 0) {
+        } else if (_.toString(data['messageText']).match(/^[0-9a-zA-z]{6}\*/)) {
             // Indentity Verification
             Customer.findOne({ lineUID: data['sourceUserId'] }).then((customer) => {
                 if (customer) {
